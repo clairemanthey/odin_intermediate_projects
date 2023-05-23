dae6ad8f65a2ea282f2781f3cdf2f83dc165f8a7
clairemanthey <clairemanthey@gmail.com>
2023-05-23 17:00:23 -0500

    added button functionality and styles

--- library/index.js
+++ library/index.js
@@ -1,6 +1,7 @@
 //Variables from HTML
 let headerOne = document.getElementById("header_01"); 
-let section = document.getElementById("section_book-list")
+let section = document.getElementById("section_book-list");
+let inputs = document.getElementsByClassName('input')
 //Both Forms
 let asideForm = document.getElementById("aside_form");
 let modalForm = document.getElementById("modal-form")
@@ -28,8 +29,6 @@ let myLibrary = [
 	];
 /*--Display Books in DOM--*/
 function Book(){
-	// Write a functiontaht loops through the array
-	//Displays each book on the page'
 
 	let displayMyLibrary = myLibrary.map(({title, author, pages, dateAdded, read}) =>{	
 		let bookTitle = title;
@@ -65,23 +64,47 @@ function Book(){
 		status.textContent = `Status: ${readBook ? "Read" : "Not Read"}`;
 		article.appendChild(status);
 
+		//created finished checkbox
+		let finishedContainer = document.createElement("div");
+		finishedContainer.classList.add("container_finished");
+		let finishedLabel = document.createElement("label");
+		finishedLabel.classList.add("label_finished")
+		finishedLabel.textContent = "Finished?";
+		let finishedInput = document.createElement("input");
+		
+		finishedInput.setAttribute("type", "checkbox");
+		finishedInput.classList.add("input_finished");
+
+		if(readBook === true){
+			finishedInput.checked = true;
+		}
+
+		finishedInput.onclick = function(){
+			if(readBook === false){
+				readBook = true
+				status.textContent = "Status: Read";
+			}else if(readBook === true){
+				readBook = false;
+  			    status.textContent = "Status: Not Read";
+			}
+		}
+
+		finishedContainer.appendChild(finishedLabel);
+		finishedContainer.appendChild(finishedInput)
+		article.appendChild(finishedContainer);
+
 		//create delete button and displayed in DOM
 		let btnDelete = document.createElement("button");
 		btnDelete.classList.add("btn-delete");
 		btnDelete.textContent = "Delete";
 		article.appendChild(btnDelete)
 
-
-
 		//Displayed article in DOM
 		section.appendChild(article)	
 	})
 
-
 }
-
 Book();
-
 /*--Add Books--*/
 
 /*input from aside*/
@@ -90,7 +113,6 @@ let inputTitle = document.getElementById("input-title");
 let inputAuthor = document.getElementById("input-author");
 let inputLength = document.getElementById("input-length");
 let inputRead = document.getElementById("input-read");
-let inputs = document.getElementsByClassName("input")
 
 /*input from modal*/
 let addBtn02 = document.getElementById("modal_add-btn");
@@ -100,45 +122,215 @@ let modalLength = document.getElementById("modal-length");
 let modalRead = document.getElementById("modal-read");
 
 
-function addBookToLibrary(){
-	console.log("BOOK ADDED")
+function addBookToLibraryInput(){
+	//generate new id
+	let lastElement = myLibrary[myLibrary.length - 1];
+	let nextId = lastElement.id + 1
+
 	//inputs from aside
 	let asideTitleValue = inputTitle.value;
-	console.log(asideTitleValue)
 	let asideAuthorValue = inputAuthor.value;
-	console.log(asideAuthorValue)
-	let asideLengthValue = inputLength.value;
-	console.log(asideLengthValue)
+	let asideLengthValue = parseInt(inputLength.value);
 	let asideReadValue = inputRead.checked;
-	console.log(asideReadValue)
 
+	myLibrary.push({
+		id:nextId, title:asideTitleValue, author:asideAuthorValue, pages:asideLengthValue,read:asideReadValue
+	})
+
+	//Display new book from aside inputs in DOM
+    //Created the article element
+    let article = document.createElement("article");
+  	 article.classList.add("article_book")
+
+	//created h3 elementt and displed the book title in the DOM
+	let asideTitle = document.createElement("h3");
+	asideTitle.classList.add("book_header");
+	asideTitle.textContent = asideTitleValue;
+	article.appendChild(asideTitle);
+
+	//created p element and diplayed author in DOM
+	let asideAuthor = document.createElement("p");
+	asideAuthor.classList.add("display_author");
+	asideAuthor.textContent = asideAuthorValue;
+	article.appendChild(asideAuthor);
+
+	//created p element and displayed number of pages in DOM
+	let asidePages = document.createElement("p");
+	asidePages.classList.add("display_pages");
+	asidePages.textContent = asideLengthValue;
+	article.appendChild(asidePages);
+
+	//created p element for read, displayed in DOM
+	let asideStatus = document.createElement("p");
+	asideStatus.classList.add("status")
+	asideStatus.textContent = `Status: ${asideReadValue ? "Read" : "Not Read"}`;
+	article.appendChild(asideStatus);
+
+	//created finished checkbox
+	let finishedContainer = document.createElement("div");
+	finishedContainer.classList.add("container_finished");
+	let finishedLabel = document.createElement("label");
+	finishedLabel.classList.add("label_finished")
+	finishedLabel.textContent = "Finished?";
+	let finishedInput = document.createElement("input");
+		
+	finishedInput.setAttribute("type", "checkbox");
+	finishedInput.classList.add("input_finished");
+
+	if(asideReadValue === true){
+		finishedInput.checked = true;
+	}
+
+	finishedInput.onclick = function(){
+		if(asideReadValue === false){
+			asideReadValue = true
+			asideStatus.textContent = "Status: Read";
+		}else if(asideReadValue === true){
+			asideReadValue = false;
+  		    asideStatus.textContent = "Status: Not Read";
+		}
+	}
+
+	finishedContainer.appendChild(finishedLabel);
+	finishedContainer.appendChild(finishedInput)
+	article.appendChild(finishedContainer);
+
+	//create delete button and displayed in DOM
+	let btnDelete = document.createElement("button");
+	btnDelete.classList.add("btn-delete");
+	btnDelete.textContent = "Delete";
+	btnDelete.onclick = function(e){
+		btnDelete.parentElement.remove();
+	}
+	article.appendChild(btnDelete)
+	section.appendChild(article)	
+}
+
+function addBookToLibraryModal(){
+    //Add book from modal input
+	//generate new id
+	let lastElement = myLibrary[myLibrary.length - 1];
+	let nextId = lastElement.id + 1
 
 	//inputs from modal
 	let modalTitleValue = modalTitle.value;
-	console.log(modalTitleValue);
 	let modalAuthorValue = modalAuthor.value;
-	console.log(modalAuthorValue);
 	let modalLengthValue = modalLength.value;
-	console.log(modalLengthValue)
 	let modalReadValue = modalRead.checked;
-	console.log(modalReadValue);
 
+	myLibrary.push({
+		id:nextId, title:modalTitleValue, author:modalAuthorValue, pages:modalLengthValue,read:modalReadValue
+	})
+	
+	//Display new book from modal inputs in DOM
+
+    //Created the article element
+
+    let article = document.createElement("article");
+  	article.classList.add("article_book")
+
+	//created h3 elementt and displed the book title in the DOM
+	let displayModalTitle = document.createElement("h3");
+	displayModalTitle.classList.add("book_header");
+	displayModalTitle.textContent = modalTitleValue;
+	article.appendChild(displayModalTitle);
+
+	//created p element and diplayed author in DOM
+	let displayModalAuthor = document.createElement("p");
+	displayModalAuthor.classList.add("display_author");
+	displayModalAuthor.textContent = modalAuthorValue;
+	article.appendChild(displayModalAuthor);
+
+	//created p element and displayed number of pages in DOM
+	let displayModalPages = document.createElement("p");
+	displayModalPages.classList.add("display_pages");
+	displayModalPages.textContent = modalLengthValue;
+	article.appendChild(displayModalPages);
+
+	//created p element for read, displayed in DOM
+	let displayModalStatus = document.createElement("p");
+	displayModalStatus.classList.add("status")
+	displayModalStatus.textContent = `Status: ${modalReadValue ? "Read" : "Not Read"}`;
+	article.appendChild(displayModalStatus);
+
+	//created finished checkbox
+	let finishedContainer = document.createElement("div");
+	finishedContainer.classList.add("container_finished");
+	let finishedLabel = document.createElement("label");
+	finishedLabel.classList.add("label_finished")
+	finishedLabel.textContent = "Finished?";
+	let finishedInput = document.createElement("input");
+		
+	finishedInput.setAttribute("type", "checkbox");
+	finishedInput.classList.add("input_finished");
+
+	if(modalReadValue === true){
+		finishedInput.checked = true;
+	}
+
+	finishedInput.onclick = function(){
+		if(modalReadValue === false){
+			modalReadValue = true
+			displayModalStatus.textContent = "Status: Read";
+		}else if(modalReadValue === true){
+			modalReadValue = false;
+  		    displayModalStatus.textContent = "Status: Not Read";
+		}
+	}
+
+	finishedContainer.appendChild(finishedLabel);
+	finishedContainer.appendChild(finishedInput)
+	article.appendChild(finishedContainer);
+
+
+	//create delete button and displayed in DOM
+	let btnDelete = document.createElement("button");
+	btnDelete.classList.add("btn-delete");
+	btnDelete.textContent = "Delete";
+	btnDelete.onclick = function(e){
+		btnDelete.parentElement.remove();
+	}
+	article.appendChild(btnDelete)
+		
+	//Displayed article in DOM
+	section.appendChild(article)	
 }
+
+//Reset all inputs
+let resetInputs = () => {
+	for (let i = 0; i < inputs.length; i++){
+		inputs[i].value = "";
+		inputs[i].checked = false;
+	}
+}
+
+//Button Event Listeners
 
 addBtn01.addEventListener("click", (event) => {
 	event.preventDefault();
-	addBookToLibrary();
+	addBookToLibraryInput();
+	resetInputs();
 })
-
-//addBtn01.addEventListener("click", addBookToLibrary)
 
 addBtn02.addEventListener("click", (event) => {
 	event.preventDefault();
-	addBookToLibrary();
+	addBookToLibraryModal();
+	resetInputs();
 
 })
 
-//addBtn02.addEventListener("click", addBookToLibrary)
+//Delete entry function
+window.addEventListener("DOMContentLoaded", function(event){
+  document.querySelectorAll('.btn-delete').forEach(el => {
+    el.onclick = () => deleteBook(el)
+  })
+});
+function deleteBook(e){
+  e.parentElement.remove()
+}
+
+//Switch from Not Read to Read
+
 
 
 /*--Display and Close add book modal--*/
